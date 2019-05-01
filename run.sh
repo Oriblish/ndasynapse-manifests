2b5164c42da02e44cfc44e0c9618066690de4fee
kdaily <kenneth.daily@sagebase.org>
2019-05-01 13:56:41 -0700

    add collections

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 -
 
 for manifest_type in genomics_subject genomics_sample nichd_btb ; do
 
-    query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2963 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}.csv ;
+    query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}.csv ;
 
 done
 
