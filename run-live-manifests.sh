cfa350f9e0d1430782c43be2c2c2a3fa71085d98
kdaily <kenneth.daily@sagebase.org>
2019-09-25 14:53:24 -0700

    remove extra redirect

--- run-live-manifests.sh
+++ run-live-manifests.sh
@@ -6,5 +6,5 @@ aws ssm get-parameters --names synapseconfig-kdaily --with-decryption --region u
 aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/ndaconfig.json
 
 for manifest_type in genomics_subject02 genomics_sample03 nichd_btb02 ; do
-    manifest_guid_data.py --config /root/ndaconfig.json --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} >| > /tmp/nda-manifests-${manifest_type}-LIVE.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-LIVE.csv;
+    manifest_guid_data.py --config /root/ndaconfig.json --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-LIVE.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-LIVE.csv;
 done
