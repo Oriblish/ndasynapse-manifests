e11a9af035a83a2bed98ab54eee99bec9a5f226b
kdaily <kenneth.daily@sagebase.org>
2019-09-26 09:23:59 -0700

    add nda version to output

--- run-live-manifests.sh
+++ run-live-manifests.sh
@@ -8,4 +8,6 @@ manifest_type=$(sed -n ${LINE}p /manifest_types.txt)
 aws ssm get-parameters --names synapseconfig-kdaily --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
 aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/ndaconfig.json
 
+echo "Running ndasynapse" $(query-nda --version) > /dev/stderr
+
 manifest_guid_data.py --config /root/ndaconfig.json --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-LIVE.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-LIVE.csv;
--- run-original-manifests.sh
+++ run-original-manifests.sh
@@ -8,4 +8,6 @@ manifest_type=$(sed -n ${LINE}p /manifest_types_short.txt)
 aws ssm get-parameters --names synapseconfig-kdaily --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
 aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/ndaconfig.json
 
+echo "Running ndasynapse" $(query-nda --version) > /dev/stderr
+
 query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
