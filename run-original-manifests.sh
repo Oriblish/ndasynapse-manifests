1fb8e1236438dc3dcce006a702cb92738b08bf03
kdaily <kenneth.daily@sagebase.org>
2019-09-25 18:55:31 -0700

    convert to array batch jobs based on manifest types

--- run-original-manifests.sh
+++ run-original-manifests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+LINE=$((AWS_BATCH_JOB_ARRAY_INDEX + 1))
+manifest_type=$(sed -n ${LINE}p /manifest_types_short.txt)
+
 # Query NDA using the original submitted manifests for all BSMN collections
 
 aws ssm get-parameters --names synapseconfig-kdaily --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
 aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/ndaconfig.json
 
-for manifest_type in genomics_subject genomics_sample nichd_btb ; do
-    query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv ;
-done
+query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv && synapse store --parentId syn11452082 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
