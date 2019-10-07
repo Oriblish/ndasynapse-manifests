8ed2b4629b82786441ec7ee2d882dddbf784dfe4
kdaily <kenneth.daily@sagebase.org>
2019-10-07 15:49:01 -0700

    update folder ids again

--- run-original-manifests.sh
+++ run-original-manifests.sh
@@ -10,4 +10,4 @@ aws ssm get-parameters --names nda-config --with-decryption --region us-east-1 -
 
 echo "Running ndasynapse" $(query-nda --version) > /dev/stderr
 
-query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv && synapse store --parentId syn20712253 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
+query-nda --config /root/ndaconfig.json get-collection-manifests --collection_id 2458 2960 2961 2962 2963 2964 2965 2966 2967 2968 --manifest_type ${manifest_type} > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv && synapse store --parentId syn20858272 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
