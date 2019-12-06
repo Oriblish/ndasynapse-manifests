cd035ec1323e8ecc2512dbe62323ba10f628fd38
kdaily <kenneth.daily@sagebase.org>
2019-12-05 16:30:32 -0800

    add script to concatenate

---
+++ concatenate-csvs.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", type=str, nargs="+")
+
+    args = parser.parse_args()
+    all_csv = [os.path.abspath(file_name) for file_name in args.files]
+ 
+    li = []
+
+    for filename in all_csv:
+        try:
+            df = pd.read_csv(filename, index_col=None, header=0, 
+                            parse_dates=False, infer_datetime_format=False)
+            li.append(df)
+        except pd.errors.EmptyDataError:
+            pass
+
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    frame.to_csv('/dev/stdout', index=False)
