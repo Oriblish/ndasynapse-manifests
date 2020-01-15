b1eb6723e7879871ddcc8e6ef97d9bc109c72a56
kdaily <kenneth.daily@sagebase.org>
2020-01-15 15:57:06 -0800

    fix sorting again

--- bin/run-original-manifests.sh
+++ bin/run-original-manifests.sh
@@ -21,6 +21,10 @@ find /tmp/ -maxdepth 1 -name "nda-manifest-${manifest_type}*-ORIGINAL.csv" -size
 # Concatenate all files together and sort the columns.
 # The columns are sorted alphabetically so that if the contents haven't changed
 # then a new version is not pushed to Synapse.
-/usr/local/bin/concatenate-csvs.py /tmp/nda-manifest-${manifest_type}-*-ORIGINAL.csv | sort | sort-columns.py | /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
+/usr/local/bin/concatenate-csvs.py /tmp/nda-manifest-${manifest_type}-*-ORIGINAL.csv | sort-columns.py > /tmp/nda-manifests-${manifest_type}-ORIGINAL-unsorted.csv
+
+# Need to sort rows too
+head -n 1 /tmp/nda-manifests-${manifest_type}-ORIGINAL-unsorted.csv > /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
+tail -n +2 /tmp/nda-manifests-${manifest_type}-ORIGINAL-unsorted.csv | sort >> /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
 
 synapse store --noForceVersion --parentId syn20858272 /tmp/nda-manifests-${manifest_type}-ORIGINAL.csv
